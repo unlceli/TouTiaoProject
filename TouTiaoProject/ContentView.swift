911a5394a43921c7d929e3a9985fcc39dfe23d54
shequ <125956902@qq.com>
2019-12-22 15:57:37 +0800

    Initial Commit

---
+++ TouTiaoProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TouTiaoProject
+//
+//  Created by lijunjun on 2019/12/22.
+//  Copyright © 2019 lijunjun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
